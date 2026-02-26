588ade171e09f9c78bae3e8804f8c882229af517
Pierre-Jeremiah Merry <pmerry02@gmail.com>
2026-02-26 10:32:20 -0500

    Initial commit with HelloWorld.java

---
+++ org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Name: Pierre Merry
+ */
+package LSP_REPO_swish.org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
